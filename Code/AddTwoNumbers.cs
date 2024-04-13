5a48f47f745accd4cb32f5f5731e2ed3a827edfc
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-04-13 10:54:15 -0500

    Add project files.

---
+++ Code/AddTwoNumbers.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        var stop = false;
+        var pointer1 = l1;
+        var pointer2 = l2;
+
+        var resultHead = new ListNode(-1);
+        var resultPointer = resultHead;
+        var leftOver = 0;
+
+        while(!stop)
+        {
+           
+            if (pointer1 == null && pointer2 == null)
+            {
+                if(leftOver > 0) 
+                {
+                    BuildResult(leftOver, resultPointer);
+                }
+                break;
+            }
+
+            var value1 = pointer1 != null ? pointer1.val : 0;
+            var value2 = pointer2 != null ? pointer2.val : 0;
+            var valueRes = (value1 + value2 + leftOver).ToString();
+            var res = valueRes.Length > 1 ? Convert.ToInt32(valueRes.Substring(1, 1)) : Convert.ToInt32(valueRes);
+            leftOver = valueRes.Length > 1 ? Convert.ToInt32(valueRes.Substring(0, 1)) : 0;
+            resultPointer = BuildResult(res, resultPointer);
+
+            pointer1 = pointer1?.next;
+            pointer2 = pointer2?.next;
+           
+        }
+
+        return resultHead;
+    }
+
+    private ListNode BuildResult(int value, ListNode head)
+    {
+        if(head.val < 0)
+        {
+            head.val = value;
+            return head;
+        }
+        var newHeadPointer = new ListNode(value);
+        head.next = newHeadPointer;
+        return newHeadPointer;
+    }
+}
---
+++ Code/LengthOfLongestSubstring.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+public class Solution 
+{
+    public int LengthOfLongestSubstring(string s) 
+    {
+        int max = 0;
+        int startPoint = 0;
+        int counter = 0;
+        var sb = new StringBuilder();
+
+        for (int i = 0; i <= s.Length - 1; i++)
+        {
+            sb.Append(s[i]);
+            counter++;
+            if(i == s.Length - 1)
+            {
+                max = counter > max ? counter : max;
+                break;
+            }
+
+            if (sb.ToString().Contains(s[i + 1].ToString()))
+            {
+                max = counter > max ? counter : max;
+                i = startPoint;
+                startPoint++;
+                counter = 0;
+                sb.Clear();
+                continue;
+            }
+        }
+
+        return max;
+    }
+}
