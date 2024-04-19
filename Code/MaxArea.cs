15cd4bb7593137b94448603650e01a44d29ddf9c
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-04-19 10:14:06 -0500

    improvements

--- Code/MaxArea.cs
+++ Code/MaxArea.cs
@@ -4,11 +4,10 @@
         var maxArea = 0;
         var n = height.Length;
         var i = 0;
-        var area = 0;
-        
+
         while (i != n - 1)
         {
-            area = (Math.Min(height[i], height[n - 1])) * (n - (i + 1));
+            var area = (Math.Min(height[i], height[n - 1])) * (n - (i + 1));
             maxArea = Math.Max(maxArea, area);
             if (height[i] > height[n - 1])
             {
