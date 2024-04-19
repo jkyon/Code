bc1fb99ae7c2cfd3ac48db24950e790621f659ad
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-04-19 10:07:27 -0500

    Add MaxArea

---
+++ Code/MaxArea.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var n = height.Length;
+        var i = 0;
+        var area = 0;
+        
+        while (i != n - 1)
+        {
+            area = (Math.Min(height[i], height[n - 1])) * (n - (i + 1));
+            maxArea = Math.Max(maxArea, area);
+            if (height[i] > height[n - 1])
+            {
+                n--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return maxArea;
+    }
+}
