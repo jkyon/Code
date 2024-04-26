6ba1965561740cbd47d9012e2212b3d58e3528dd
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-04-26 08:07:32 -0500

    Add ClimbStairs and RemoveNth solutions

---
+++ Code/ClimbStairs.cs
@@ -0,0 +1,29 @@
+﻿public class Solution 
+{
+    public int ClimbStairs(int n) 
+    {
+        var cache = new Dictionary<int, int> ();
+        var result = Steps(n + 1, cache);
+        return result;
+    }
+
+    private int Steps(int i, Dictionary<int, int> cache)
+    {
+        if(i == 0)
+        {
+            return 0;
+        }
+
+        if(i == 1)
+        {
+            return 1;
+        }
+
+        if(!cache.ContainsKey(i))
+        {
+            cache.Add(i, Steps(i - 1, cache) + Steps(i - 2, cache));
+        }
+
+        return cache[i];
+    }
+}
---
+++ Code/RemoveNthFromEnd.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) 
+    {
+        var pointer1 = head;
+        var pointer2 = head;
+        var count = 0;
+
+        while (pointer2?.next != null)
+        {
+            if (count != n)
+            {
+                pointer2 = pointer2?.next;
+                count++;
+                continue;
+            }
+
+            pointer2 = pointer2?.next;
+            pointer1 = pointer1?.next;
+        }
+
+        if(count == n - 1)
+        {
+            pointer1 = pointer1?.next;
+            return pointer1;
+        }
+
+        pointer1.next = pointer1?.next?.next;
+        return head;
+    }
+}
