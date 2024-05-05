851f32de638da915e8352093d4c1de81f42440e5
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-05 08:40:03 -0500

    Solution for "Find First and Last Position of Element in Sorted Array"

---
+++ Code/SearchRange.cs
@@ -0,0 +1,49 @@
+﻿public class Solution 
+{
+    public int[] SearchRange(int[] nums, int target) 
+    {
+        var resultSet = new int[2];
+
+        resultSet[0] = FindOccurence(nums, target,true);
+
+        resultSet[1] = FindOccurence(nums, target,false);
+
+        return resultSet;
+    }
+
+    private int FindOccurence(int[] nums, int target, bool findOnLeft)
+    {
+        var pLeft = 0;
+        var pRight = nums.Length - 1;
+        var middle = 0;
+        var result = -1;
+
+        while (pLeft <= pRight)
+        {
+            middle = (pLeft + pRight) / 2;
+
+            if (nums[middle] > target)
+            {
+                pRight = middle - 1;
+            }
+            else if (nums[middle] < target)
+            {
+                pLeft = middle + 1;
+            }
+            else
+            {
+                result = middle;
+                if(findOnLeft)
+                {
+                    pRight = middle - 1;
+                }
+                else
+                {
+                    pLeft = middle + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
