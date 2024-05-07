8fb267fd995a4e286fa878b1bbf32bb2af0f078e
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-07 06:49:45 -0500

    Add MissingNumbers and TwoSum2 solutions

---
+++ Code/MissingNumber.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int MissingNumber(int[] nums) {
+        var total = 0;
+        var subTotal = 0;
+        
+        for(var i = 1; i <= nums.Length; i++)
+        {
+            total += i;
+            subTotal += nums[i - 1];
+            
+        }
+        return total - subTotal;
+    }
+}
---
+++ Code/TwoSum2.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] numbers, int target) 
+    {
+        var result = new int[2];
+        var pointer1 = 0;
+        var pointer2 = numbers.Length - 1;
+
+        while (pointer1 <= pointer2)
+        {
+            var sum = numbers[pointer1] + numbers[pointer2];
+            if(sum == target)
+            {
+                result[0] = pointer1 + 1;
+                result[1] = pointer2 + 1;
+                break;
+            }
+
+            if(sum > target)
+            {
+                pointer2--;
+                continue;
+            }
+            
+            pointer1++;
+        }
+        
+        return result;
+    }
+}
