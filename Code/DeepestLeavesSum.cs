b89d084e327ba67a20c8017905701c1de14fcefd
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-11 18:11:01 -0500

    Add DeepestLeavesSum

---
+++ Code/DeepestLeavesSum.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    private int totalSum = 0;
+
+    private int maxLevel = -1;
+
+    public int DeepestLeavesSum(TreeNode root) 
+    {
+        Travel(root, 1);
+        return totalSum;
+    }
+
+    private void Travel(TreeNode node, int level)
+    {
+        if(node == null)
+        {
+            return;
+        }
+
+        if(node.left == null && node.right == null)
+        {
+            if(level > maxLevel)
+            {
+                maxLevel = level;
+                totalSum = node.val;
+            }
+            else if(maxLevel == level)
+            {
+                totalSum = totalSum + node.val;
+            }
+        }
+
+        level++;
+        Travel(node.left, level);
+
+        Travel(node.right, level);
+    }
+
+
+}
