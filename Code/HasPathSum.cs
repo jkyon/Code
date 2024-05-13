e477f02b2cadf0feb882d72b65efeee7cc6a1de2
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-12 21:23:16 -0500

    Add HasPathSum solution

---
+++ Code/HasPathSum.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    private int targetSumValue = 0;
+    private bool found = false;
+    public bool HasPathSum(TreeNode root, int targetSum) 
+    {
+        targetSumValue = targetSum;
+        Travel(root, root?.val ?? 0);
+        return found;
+    }
+
+    private void Travel(TreeNode node, int currentSum)
+    {
+        if(node == null)
+        {
+            return;
+        }
+
+        if(found)
+        {
+            return;
+        }
+
+    
+        if(node.left == null && node.right == null)
+        {
+            if(targetSumValue == currentSum)
+            {
+                found = true;
+            }
+        }
+
+        Travel(node.left, currentSum + node.left?.val ?? 0);
+
+        Travel(node.right, currentSum + node.right?.val ?? 0);
+    }
+
+}
