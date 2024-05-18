ba8854508fbd9688da21651232b5b87ebb34a2fc
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-18 06:17:17 -0500

    add maxSubArray solution

---
+++ Code/MaxSubArray.cs
@@ -0,0 +1,20 @@
+﻿public class Solution 
+{
+    public int MaxSubArray(int[] nums) 
+    {
+        var max = nums[0];
+        var sumAcum = 0;
+
+        foreach(var num in nums)
+        {
+            if(sumAcum < 0)
+            {
+                sumAcum = 0;
+            }
+            sumAcum += num;
+            max = Math.Max(sumAcum, max);
+        }
+
+        return max;
+    }
+}
