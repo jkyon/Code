5a194f6802b09c074b5922c04b39dc9c61e33502
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-20 09:13:47 -0500

    add TwoSum

---
+++ Code/TwoSum.cs
@@ -0,0 +1,23 @@
+﻿public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        var result = new int[2];
+        var dictionary = new Dictionary<int, int>();
+
+        for(var i = 0; i < nums.Length; i++)
+        {
+            if(dictionary.ContainsKey(nums[i]))
+            {
+                result[0] = dictionary[nums[i]];
+                result[1] = i;
+                break;
+            }
+
+            dictionary.TryAdd(target - nums[i], i);
+        }
+
+
+        return result;
+    }
+}
