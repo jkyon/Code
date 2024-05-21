30d4cdb98d64c1264d9d9f0eca01d61f9672d2cb
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-05-21 08:39:13 -0500

    Add 3Sum solution

--- Code/3Sum.cs
+++ Code/3Sum.cs
@@ -2,42 +2,43 @@
     public IList<IList<int>> ThreeSum(int[] nums) 
     {
         Array.Sort(nums);
-        var collection = new List<IList<int>>();
-        var i = 0;
-        var left = i + 1;
-        var right = nums.Length - 1;
-        var hashSet = new HashSet<string>();
-        while (i != left && i != right && left != right)
+        var result = new List<IList<int>>();
+        var pointer1 = 1;
+
+        for(var x = 0; x < pointer1; x++)
         {
-            if (nums[i] + nums[left] + nums[right] == 0)
+            if(x > 0 && nums[x] == nums[x - 1]) 
             {
-                var triplet = $"{nums[i]},{nums[left]},{nums[right]}";
-                if(!hashSet.Contains(triplet))
+                continue;
+            }
+
+            pointer1 = x + 1;
+            var pointer2 = nums.Length - 1;
+            
+            while (pointer1 < pointer2)
+            {
+                var sum = nums[x] + nums[pointer1] + nums[pointer2];
+                if(sum == 0)
                 {
-                    collection.Add(new List<int>() { nums[i], nums[left], nums[right] });
-                    hashSet.Add(triplet);
-                }
-                if(right - 1 == left)
-                {
-                    i++;
-                    left = i + 1 >= right ? left :  i + 1;
+                    result.Add(new List<int> { nums[x], nums[pointer1], nums[pointer2] });
+                    pointer1++;
+                    while(nums[pointer1] == nums[pointer1 - 1] && pointer1 < pointer2)
+                    {
+                        pointer1++;
+                    }
                     continue;
                 }
-                right = right - 1 == left ? right : right - 1;
-                continue;
+
+                if(sum > 0)
+                {
+                    pointer2--;
+                    continue;
+                }
+                pointer1++;
             }
 
-            if(left + 1 == right)
-            {
-                i++;
-                left = i + 1 >= right ? left : i + 1;
-                continue;
-            }
-
-            left = left + 1 == right ? left : left + 1;
-            
         }
-
-        return collection;
+        
+        return result;
     }
 }
