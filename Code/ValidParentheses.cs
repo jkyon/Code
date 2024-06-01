14246f2b76ef18e7bcbb4c82343463604d5fa789
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-06-01 10:04:33 -0500

    Add validParentheses solution

---
+++ Code/ValidParentheses.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public bool IsValid(string s) 
+    {
+        var dictionary = new Dictionary<char, char> {
+            {'{', '}'},
+            {'[', ']'},
+            {'(', ')'},
+        };
+        var stack = new Stack<char>();
+        
+        foreach(var bracket in s)
+        {
+            if(dictionary.ContainsKey(bracket))
+            {
+                stack.Push(bracket);
+                continue;
+            }
+
+            if(!stack.TryPeek(out var peekItem))
+            {
+                return false;
+            }
+
+            if(dictionary.ContainsKey(peekItem) && dictionary[peekItem] == bracket)
+            {
+                stack.Pop();
+                continue;
+            }
+            return false;
+        }
+        
+
+        return stack.Count <= 0;
+    }
+}
