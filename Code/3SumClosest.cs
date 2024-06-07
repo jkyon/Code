15465aaedafa2c506b55f4b03c1133cf1353e154
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-06-07 10:08:58 -0500

    Add ThreeSumClosest

---
+++ Code/3SumClosest.cs
@@ -0,0 +1,36 @@
+﻿public class Solution 
+{
+    public int ThreeSumClosest(int[] nums, int target) 
+    {
+        var closest = 100001;
+        Array.Sort(nums);
+        
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            var pointer1 = i + 1;
+            var pointer2 = nums.Length - 1;
+            while (pointer1 < pointer2)
+            {
+                var sum = nums[i] + nums[pointer1] + nums[pointer2];
+                if (sum == target)
+                {
+                    return sum;
+                }
+
+                if (Math.Abs(target - sum) <  Math.Abs(target - closest))
+                {
+                    closest = sum;
+                }
+                
+                if(sum < target)
+                {
+                    pointer1++;
+                    continue;
+                }
+
+                pointer2--;
+            }	
+        }
+        return closest;
+    }
+}
