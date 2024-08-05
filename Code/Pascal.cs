1b5b3208128f192806a6677e067a50a0f8aebf95
Juan Orjuela <orjuela.juanmanuel@gmail.com>
2024-08-05 09:39:59 -0500

    Pascal Solution

---
+++ Code/Pascal.cs
@@ -0,0 +1,20 @@
+﻿public class Pascal {
+    public IList<IList<int>> Generate(int numRows) 
+    {
+        var resultSet = new List<IList<int>>();
+
+        for(var row = 1; row <= numRows; row++)
+        {
+            var rowValues = new List<int>();
+            var coeffcient = 1;
+            for(var rowPos = 1; rowPos <= row; rowPos++)
+            {
+                rowValues.Add(coeffcient);
+                coeffcient = coeffcient * (row - rowPos) / rowPos;
+            }
+            resultSet.Add(rowValues);
+        }
+
+        return resultSet;
+    }
+}
